17881193001c4ee99806c0cd54f24f6d408387ef
GrinbergD <grinberg2719@gmail.com>
2019-08-23 23:38:28 +0300

    исправление ошибок

--- src/Lesson1/Variable.java
+++ src/Lesson1/Variable.java
@@ -2,24 +2,27 @@ package Lesson1;
 
 public class Variable {
 
-public static void main(String[] args) {
-    	 boolean graficsOnBoard = true;
-    	 byte core = 2;
-    	 char  modem = ADSL;
-    	 double frequecy = 1.8;
-    	 float ram = 2048.0;
-    	 int flashMemory = 2;
-    	 long kashMemory = 16;
-    	 short cd = 500;
+    private static final char ADSL = 'a';
 
-       	 System.out.println(a1);
-         System.out.println(core);
-         System.out.println(modem);
-         System.out.println(frequecy);
-         System.out.println(ram);
-         System.out.println(flashMemory);
-         System.out.println(kashMemory);
-         System.out.println(cd);
+    public static void main(String[] args) {
 
-	}
+        boolean graficsOnBoard = true;
+        byte core = 2;
+        char modem = ADSL;
+        double frequecy = 1.8;
+        float ram = (float) 2048.0;
+        int flashMemory = 2;
+        long kashMemory = 16;
+        short cd = 500;
+
+        System.out.println(graficsOnBoard);
+        System.out.println(core);
+        System.out.println(modem);
+        System.out.println(frequecy);
+        System.out.println(ram);
+        System.out.println(flashMemory);
+        System.out.println(kashMemory);
+        System.out.println(cd);
+
+    }
 }
