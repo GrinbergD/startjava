cd07784d6ceda81a2bd9205c6ec26e61cf24fc1b
GrinbergD <grinberg2719@gmail.com>
2019-08-24 00:55:15 +0300

    исправление ошибок

--- src/Lesson1/Variable.java
+++ src/Lesson1/Variable.java
@@ -3,18 +3,18 @@ package Lesson1;
 public class Variable {
 
     public static void main(String[] args) {
-        boolean isgraficsOnBoard = true;
+        boolean isGraficsOnBoard = true;
         byte core = 2;
-        char DvD = '7';
+        char dvd = '7';
         double frequecy = 1.8;
         float ram = 2048.0f;
         int flashMemory = 2;
         long cashMemory = 16;
         short cd = 500;
 
-        System.out.println(isgraficsOnBoard);
+        System.out.println(isGraficsOnBoard);
         System.out.println(core);
-        System.out.println(DvD);
+        System.out.println(dvd);
         System.out.println(frequecy);
         System.out.println(ram);
         System.out.println(cashMemory);
