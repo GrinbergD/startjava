ec8e0dba64b41fcfc4f8917e77371fc4ca5532be
GrinbergD <grinberg2719@gmail.com>
2019-08-24 23:31:06 +0300

    исправление ошибок

--- src/Lesson1/ConditionalStatement.java
+++ src/Lesson1/ConditionalStatement.java
@@ -4,39 +4,37 @@ package Lesson1;
 public class ConditionalStatement {
 
     public static void main(String[] args) {
-        int myYears = 15;
-        if (myYears > 20) {
+        int myAge = 15;
+        if (myAge > 20) {
             System.out.println("Нет, не верно");
         }
 
-        boolean men = true;
-        boolean women = true;
-        if (men) {
+        boolean isMen = true;
+        if (isMen) {
 
             System.out.println("Yes");
         }
 
-        if (women) {
+        if (!isMen) {
             System.out.println("No");
         }
 
-        double mySize = 1.81;
+        double myHeight = 1.81;
 
-        if (mySize < 1.80) {
+        if (myHeight < 1.80) {
             System.out.println("Нет, не верно");
         } else {
             System.out.println("Да, верно");
         }
 
-        char myName = 'Д';
+        char firstLetterOfName = 'Д';
 
-        if (myName == 'M') {
+        if (firstLetterOfName == 'M') {
             System.out.println("Нет");
-        } else if (myName == 'И') {
+        } else if (firstLetterOfName == 'И') {
             System.out.println("No");
         } else {
             System.out.println("Мое имя начинается с другой буквы");
         }
     }
-
 }
